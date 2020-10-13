676c7d2a3bf4d3582ea8d5a7e7e4ebe18c2ab0e0
Lucas Assis Rodrigues <lucas@journiapp.com>
2020-10-13 17:43:40 +0200

    Initial Commit

---
+++ State/State.h
@@ -0,0 +1,18 @@
+//
+//  State.h
+//  State
+//
+//  Created by Lucas Assis Rodrigues on 13.10.20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for State.
+FOUNDATION_EXPORT double StateVersionNumber;
+
+//! Project version string for State.
+FOUNDATION_EXPORT const unsigned char StateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <State/PublicHeader.h>
+
+
