a65c3896de6196b223a84c7442ee748b3b46c5a4
Lucas Assis Rodrigues <lucas.assis.ro@gmail.com>
2026-09-06 22:51:57 -0400

    build: add swift-docc-plugin
    The plugin needs tools version 5.6, so the manifest moves up from 5.3.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,6 +10,9 @@ let package = Package(
       name: "StateMachine",
       targets: ["StateMachine"]
     ),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-docc-plugin.git", .upToNextMajor(from: "1.4.3")),
   ],
   targets: [
     .target(
